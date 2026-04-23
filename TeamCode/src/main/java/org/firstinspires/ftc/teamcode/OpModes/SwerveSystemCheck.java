d95d728c5888fe23a49716f3542fe473d492d81e
ggbob <71797845+person1789@users.noreply.github.com>
2026-04-23 18:35:06 -0400

    add Limelight folder with implementation plan, Limelight.java, LimelightLocalizer.java stubs; clean up OpMode locations and remove pedro pathing files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SwerveSystemCheck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SwerveSystemCheck.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Swerve.OpModes;
+package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/Hardware/Limelight/LimelightLocalizer.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Swerve.Hardware.Limelight;
+
+public class LimelightLocalizer extends Limelight {
+
+}
